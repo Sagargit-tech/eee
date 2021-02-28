faf36cd875e0ce9adbc4048e1e5504663dcab45a
Sagargit-tech <78584787+Sagargit-tech@users.noreply.github.com>
2021-02-28 13:44:48 +0530

    Add files via upload

---
+++ sgr.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+
+# Syntax
+
+window.sessionStorage
+# Syntax for SAVING data to sessionStorage:
+local.setItem("key", "value");
+# Syntax for READING data from sessionStorage:
+
+var c= locaStorage.getItem("key");
+# Syntax for REMOVING saved data from sessionStorage:
+
+localStorage.removeItem("key");
+# Syntax for REMOVING ALL saved data from sessionStorage:
+
+sessionStorage.clear();
